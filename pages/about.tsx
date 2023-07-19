dd79d0d0c71559dc3a523647b341a46cee6e9c7a
Joshduncan89 <51183094+Joshduncan89@users.noreply.github.com>
2023-07-19 10:55:16 -0400

    text change

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,9 +7,9 @@ const About = () => {
       <Header />
       <div className="flex h-screen w-screen bg-slate-100">
         <div className="mx-auto flex max-w-xl flex-col items-center space-y-7 p-5">
-          <h3 className="my-5 text-3xl font-semibold">About Us</h3>
+          <h3 className="my-5 text-3xl font-semibold">About</h3>
           <p>
-            EarthNews is not a real website to get your actual news. Although
+            ApocNews is not a real website to get your actual news. Although
             some of the stories dont seem far fetched from where out society may
             be heading, Bezos Bots <span className="italic">probably</span> wont
             be available for awhile. This was created solely as satire as a
